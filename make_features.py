ce70e6d879ef3592c79275ecd595265b9aa8df99
miho <km340513@gmail.com>
2020-12-01 12:31:59 +0900

    make m_f.py

---
+++ make_features.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+names = ['aizawa', 'doi', 'hayashi', 'ipei', 'karin', 
+        'matsuyama', 'mitsuki', 'nanri', 'mnonaka', 'risa',
+        'shimizu', 'ueki', 'yamazaki', 'yuka', 'yukina', 'yurichu']
+types = ['b', 'l1', 'l2', 'l3', 'l4', 'p1', 'p2', 'p3', 'p4', 'c1', 'c2', 'c3', 'c4']
+parts = ['f','c'] # f=額，c=頬
+angles = ['f','l','r', 'u','d']
